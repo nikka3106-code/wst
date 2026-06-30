5e79c105ca08c7354e1308a4bf3bbfe9b5102f28
nikka3106-code <nikka3106@gmail.com>
2026-06-30 16:34:10 +0800

    Update db.php

--- config/db.php
+++ config/db.php
@@ -11,4 +11,8 @@ $conn = new mysqli($host, $user, $pass, $db, $port);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-?>
+
+function getDB() {
+    global $conn;
+    return $conn;
+}
